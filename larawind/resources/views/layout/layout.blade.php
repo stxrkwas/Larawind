cbc765c346ae4df881351898d5fdbf4307f63303
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-06-02 13:48:48 -0300

    Cansei

--- larawind/resources/views/layout/layout.blade.php
+++ larawind/resources/views/layout/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Layout</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </head>
 
@@ -19,7 +19,7 @@
 
                 <!-- Imagem:  -->
                 <img src=".img/eteclogo.png" class="h-12" alt="Loogo da ETEC Zona Leste"> <!-- class="h-12" define o tamanho da imagem. -->
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Atividade de PW III </span>
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
@@ -46,6 +46,10 @@
             </div>
         </div>
     </nav>
+
+    <!-- Imagem: -->
+
+    <img class="h-auto max-w-full mx-auto" src=".img/home.etec.webp" alt="Imagem da ETEC Zona Leste">
 
 </body>
 
