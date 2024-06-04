7b3aec2b540cc5e5c570e1a957b4a5c3e4ef3659
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-06-04 16:41:31 -0300

    Colunas e parágrafos, I can't take it anymore.

--- larawind/resources/views/home.blade.php
+++ larawind/resources/views/home.blade.php
@@ -1,4 +1,87 @@
-@extends('layout.layout')
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- Imagem: -->
-<img class="h-auto max-w-full mx-auto" src=".img/home.etec.webp" alt="Imagem da ETEC Zona Leste">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    @extends('layout.layout')
+    <!-- Imagem: -->
+    <!--img class="h-65 max-w-xxl" src=".img/image.png" alt="Imagem da ETEC Zona Leste"-->
+    <!--img src=".img/image.png" class="img-fluid" alt="Foto tirada do campus da ETEC Zona Leste"-->
+
+
+    <figure class="relative max-w-full transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+        <a href="#">
+            <img class="" src=".img/image.png" alt="image description">
+        </a>
+        <!--figcaption class="absolute px-4 text-lg text-white bottom-6">
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Maecenas porttitor congue massa. <br>Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero,
+                sit amet commodo magna eros quis urna.</p>
+        </!--figcaption>-->
+    </figure>
+
+    <!-- Grid: -->
+    <div class="container text-center">
+
+        <!-- Linha: -->
+        <div class="row">
+
+            <!-- Coluna da esquerda: -->
+            <div class="col">&nbsp;</div>
+
+            <!-- Coluna central: -->
+            <div class="col">
+
+                <!-- Título -->
+                <h1 class="mb-4 my-6 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                    Nunc viverra imperdiet enim.</h1>
+
+                <!-- Parágrafo:  -->
+                <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Maecenas porttitor congue massa. <br>Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero,
+                    sit amet commodo magna eros quis urna.</p>
+
+                <!-- Botão:  -->
+                <a href="quemsomos" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg 
+                hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Saiba mais
+                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg>
+                </a>
+            </div>
+
+            <!-- Coluna direita: -->
+            <div class="col">&nbsp;</div>
+
+        </div>
+    </div>
+
+    <!-- Grid no Tailwind: -->
+    <div class="grid grid-cols-3">
+        <diiv class="row">
+            <div class="col span-1">&nbsp;</div>
+            <div class="col span-12">
+                <p class="text-justify text-lg break-words font-sans">Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                    Proin pharetra nonummy pede. Mauris et orci.
+                </p>
+                <p class="text-justify text-lg break-words font-sans">Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.</p>
+                <p class="text-justify text-lg break-words font-sans">Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis,
+                    nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.</p>
+            </div>
+            <div class="col span-1">&nbsp;</div>
+    </div>
+    </div>
+
+
+</body>
+
+</html>
--- larawind/resources/views/layout/layout.blade.php
+++ larawind/resources/views/layout/layout.blade.php
@@ -37,7 +37,7 @@
                         <a href="/home" class="block py-2 px-3 text-white text-lg bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 px-3 text-gray-900 text-lg rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Quem somos</a>
+                        <a href="/quemsomos" class="block py-2 px-3 text-gray-900 text-lg rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Quem somos</a>
                     </li>
                     <li>
                         <a href="#" class="block py-2 px-3 text-gray-900 text-lg rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contato</a>
