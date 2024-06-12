c2641a0c74db705cdbb0af941f717dccdc1a52a5
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-06-12 12:57:27 -0300

    Layout das views finalizados, views 'Contatos' e 'Quem somos' totalmente comentadas

--- larawind/resources/views/contato.blade.php
+++ larawind/resources/views/contato.blade.php
@@ -1,16 +1,138 @@
+<!-- Indicando o formato do arquivo -->
 <!DOCTYPE html>
-<html lang="en">
+
+<!-- Definindo a linguagem da página -->
+<html lang="pt-br">
+
+<!-- Cabeçalho -->
 <head>
+
+    <!-- Tipo de caracteres -->
     <meta charset="UTF-8">
+
+    <!-- ..., largura da página proporcional ao tamanho da tela e o zoom inicial. -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Título da página, aparece na barra de navegação -->
     <title>Contato</title>
+
+<!-- Fim do cabeçalho -->
 </head>
+
 <body>
-    
+
+    <!-- Estilização da página -->
+    <style>
+
+        /* Id do formulário */
+        #form1 {
+
+            /* Margem de cima */
+            padding-top: 150px;
+
+        }
+
+    /* Fim da estilização */
+    </style>
+
+    <!-- Extende o arquivo 'layout' da pasta 'layout' -->
     @extends('layout.layout')
 
+    <!-- Seção, conteúdo que fica dentro da extensão de 'content' de 'layout.layout' -->
     @section('content')
+
+    <!-- Formulário -->
+    <form class="max-w-md mx-auto" id="form1">
+
+        <!-- Div do campo de email -->
+        <div class="relative z-0 w-full mb-5 group">
+
+            <!-- Campo do email -->
+            <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            
+            <!-- Rotulo do campo 'email' -->
+            <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Endereço de email</label>
+        </div>
+
+        <!-- Div do campo de senha-->
+        <div class="relative z-0 w-full mb-5 group">
+            
+            <!-- Campo da senha -->
+            <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            
+            <!-- Rotulo do campo 'senha' -->
+            <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Senha</label>
+        </div>
+
+        <!-- Div do campo -->
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            
+            <!--  -->
+            <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirme a senha</label>
+        </div>
+
+        <!-- Div dos campos de nome  -->
+        <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="relative z-0 w-full mb-5 group">
+
+                <!-- Campo de texto -->
+                <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                
+                <!-- Rotulo do campo 'Primeiro nome' -->
+                <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Primeiro nome</label>
+            </div>
+            <div class="relative z-0 w-full mb-5 group">
+
+                <!-- Campo de texto -->
+                <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                
+                <!-- Rotulo do campo 'Sobrenome' -->
+                <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Sobrenome</label>
+            </div>
+        </div>
+
+        <!-- Div do campo -->
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="tel" pattern="+[0-9]{2} ([0-9]){2} [0-9]{5}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            
+            <!-- Rotulo do campo 'Telefone/celular': -->
+            <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Número de telefone/celular</label>
+        </div>
+
+        <div class="relative z-0 w-full mb-5 group">
+
+            <!-- Rotulo do campo de seleção 'Sexo' -->
+            <label for="sexo" class="block mb-2 text-sm font-normal text-gray-500 dark:text-white">Sexo</label>
+            
+            <!-- Lista de opções -->
+            <select id="sexo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                
+                <!-- Opção 1 -->
+                <option>Selecione uma opção</option>
+
+                <!-- Opção 'feminino' -->
+                <option>Feminino</option>
+
+                <!-- Opção 'masculino' -->
+                <option>Masculino</option>
+
+            </select>
+
+        </div>
+
+        <!-- Botão -->
+        <button type="submit" class="text-white bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-yellow-200 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-yellow-300 dark:hover:bg-yellow-400 dark:focus:ring-yellow-500">Enviar</button>
+
+        <!-- Fim do formulário -->
+    </form>
+
+    <!-- Fim da seção -->
     @endsection
-    
+
+    <!-- Final do corpo -->
 </body>
+
+<!-- Fim do HTML -->
+
 </html>
--- larawind/resources/views/quemsomos.blade.php
+++ larawind/resources/views/quemsomos.blade.php
@@ -1,31 +1,59 @@
+<!-- Indicando o formato do arquivo -->
 <!DOCTYPE html>
-<html lang="en">
 
+<!-- Definindo a linguagem da página -->
+<html lang="pt-br">
+
+<!-- Cabeçalho -->
 <head>
+
+    <!-- Tipo de caracteres -->
     <meta charset="UTF-8">
+
+    <!-- Indica que largura da página proporcional ao tamanho da tela e define o zoom inicial. -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Título da página, aparece na barra de navegação -->
     <title>Quem somos </title>
+
+<!-- Fim do cabeçalho -->
 </head>
 
-<body class="content-center">
+<!-- Corpo do HTML -->
+<body>
 
+    <!-- Estilização da página -->
     <style>
+
+        /* Imagem */
         #image {
+
+            /* Margem superior */
             padding-top: 20px;
+
+            /* Margem inferior */
             padding-bottom: 30px;
         }
 
+        /* Div 1 */
         #div1 {
+
+            /* Margem superior */
             padding-top: 80px;
         }
 
+        /* Parágrafo 1 */
         #p1{
+
+            /* Margem superior */
             padding-top: 20px;
         }
     </style>
 
+    <!-- Extende o arquivo 'layout' da pasta 'layout' -->
     @extends('layout.layout')
 
+    <!-- Seção, conteúdo que fica dentro da extensão de 'content' de 'layout.layout' -->
     @section('content')
 
     <!-- Div container: -->
@@ -40,22 +68,35 @@
         <p class="mb-4 text-lg font-normal text-center text-gray-500 lg:text-xl sm:px-24 xl:px-48 dark:text-gray-400">
             3º NOVOTEC Desenvolvimento de Sistemas. Turma da manhã. Grupo B.</p>
 
-        <!-- Imagem -->
+        <!-- Div da imagem -->
         <div class="mt-5" id="image">
+
+            <!-- Imagem -->
             <img class="rounded-full w-64 h-64 mx-auto" src="/.img/image.png" alt="image description">
+        
         </div>
 
-        <!-- Botão:  -->
+        <!-- Div do botão:  -->
         <div class="text-center mb-4">
+
+            <!-- Botão -->
             <a href="https://github.com/stxrkwas" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-yellow-300 rounded-lg 
                 hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-200 dark:focus:ring-yellow-500">Saiba mais
+                
+                <!-- Elemento raiz da imagem vetorial escalável (SVG) -->
                 <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    
+                    <!-- Este elemento define a forma real do ícone usando comandos de caminho.  -->
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                
                 </svg>
             </a>
         </div>
 
+        <!-- Div do parágrafo -->
         <div class="content-center">
+
+            <!-- Parágrafo -->
             <p class="sm:px-16 text-justify text-lg break-words font-normal" id="p1">
             Ut lectus dolor, venenatis a augue et, vestibulum dictum tortor. In vitae odio imperdiet, condimentum sem a, pretium quam. 
             Morbi erat elit, hendrerit quis maximus quis, gravida quis elit. Pellentesque habitant morbi tristique senectus et netus et 
@@ -63,11 +104,15 @@
             dignissim in cursus non, ultricies ut neque. Maecenas quis blandit nunc. In at dolor varius, fermentum enim in, dapibus tellus. 
             Fusce auctor eleifend erat. Donec dignissim eros nec erat molestie, a condimentum enim porttitor. Phasellus sapien justo, 
             pulvinar eget hendrerit a, sodales sit amet mauris. Mauris fermentum tristique massa, et fermentum lectus. 
-            Vestibulum sollicitudin ullamcorper consectetur.
-            </p>
+            Vestibulum sollicitudin ullamcorper consectetur.</p>
+
         </div>
 
+        <!-- Fim da seção -->
         @endsection
+
+<!-- Fim do corpo da página -->
 </body>
 
+<!-- Fim do arquivo -->
 </html>
